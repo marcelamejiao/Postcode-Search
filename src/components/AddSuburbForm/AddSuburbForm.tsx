ba3df90c1e0ca1ae4dfa3fd347d02894aac3d9a8
marcelamejiao <mejiao24@hotmail.com>
2024-01-10 20:46:03 +1100

    style the form to create a suburb

--- src/components/AddSuburbForm/AddSuburbForm.tsx
+++ src/components/AddSuburbForm/AddSuburbForm.tsx
@@ -37,48 +37,61 @@ export default function AddSuburnForm({ setAdded, added }: Props) {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h2>Create a suburb</h2>
-                <div>
-                    <label>Suburb's name:</label>
+        <div className="bg-dark font-roboto flex justify-center">
+            <form onSubmit={handleSubmit(onSubmit)} className="m-0 flex flex-col items-center w-3/5 ">
+                <h2 className="text-white text-5xl tracking-[.05em] mt-4">Create a Suburb</h2>
+                <div className="flex flex-col w-4/5 items-center mt-6">
+                    <div className="w-4/5 flex flex-row justify-items-start">
+                        <label className="text-white">Suburb's name:</label>
+                    </div>
                     <input
+                        className="rounded-sm w-4/5 mt-2 p-2"
                         {...register("name",
                             {required: true}
                         )}
                         aria-invalid={errors.name ? "true" : "false"}
+
                     />
                     {errors.name?.type === "required" && (
-                        <p role="alert">
-                            Name is required
-                        </p>
+                        <div className="w-4/5 flex flex-row justify-items-start">
+                            <p className="text-red" role="alert">
+                                Name is required
+                            </p>
+                        </div>
                     )}
                 </div>
-                <div>
-                    <label>Suburb's postcode:</label>
+                <div className="flex flex-col w-4/5 mt-4 items-center">
+                    <div className="w-4/5 flex flex-row justify-items-start">
+                        <label className="text-white">Suburb's postcode:</label>
+                    </div>
                     <input
+                        className="rounded-sm w-4/5 mt-2 p-2"
                         {...register("postcode",
                             {required: true}
                         )}
                         aria-invalid={errors.postcode ? "true" : "false"}
                     />
                     {errors.postcode?.type === "required" && (
-                        <p role="alert">
-                            Postcode is required
-                        </p>
+                        <div className="w-4/5 flex flex-row justify-items-start">
+                            <p className="text-red" role="alert">Postcode is required</p>
+                        </div>
                     )}
                 </div>
-                <div>
+                <div className="flex flex-col w-4/5 mt-8 items-center">
                     <input
+                        className="bg-fucsia p-3 text-white rounded-sm uppercase tracking-[.50em] w-4/5 hover:bg-fucsiaHover cursor-pointer"
                         type="submit"
                         value="Save"
                     />
                 </div>
-                <input
-                    type="button"
-                    onClick={() => reset()}
-                    value="Reset"
-                />
+                <div className="flex flex-col w-4/5 mt-6 items-center">
+                    <input
+                        className="bg-white p-3 text-dark rounded-sm w-4/5 mb-6 tracking-[.25em] hover:bg-grey cursor-pointer"
+                        type="button"
+                        onClick={() => reset()}
+                        value="Reset"
+                    />
+                </div>
             </form>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     "./src/**/*.{ts,tsx}"
   ],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif" ]
+    },
+    colors: {
+      white: '#fafafa',
+      red: '#ef4444',
+      fucsia: '#ec5990',
+      fucsiaHover: '#ec4899',
+      dark: '#0e101c',
+      grey: '#d4d4d4',
+    },
     extend: {},
   },
   plugins: [],
