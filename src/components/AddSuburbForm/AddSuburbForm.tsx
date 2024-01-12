c37adae2f37da0148041ca1b8307271165e9086d
marcelamejiao <mejiao24@hotmail.com>
2024-01-12 18:34:40 +1100

    implement mobile responsivness with tailwind

--- src/components/AddSuburbForm/AddSuburbForm.tsx
+++ src/components/AddSuburbForm/AddSuburbForm.tsx
@@ -38,14 +38,14 @@ export default function AddSuburnForm({ setAdded, added }: Props) {
 
     return (
         <div className="flex justify-center">
-            <form onSubmit={handleSubmit(onSubmit)} className="m-0 flex flex-col items-center w-3/5 ">
-                <h2 className="text-white text-5xl tracking-[.05em] mt-4 font-bold">Create a Suburb</h2>
-                <div className="flex flex-col w-4/5 items-center mt-6">
-                    <div className="w-4/5 flex flex-row justify-items-start">
+            <form onSubmit={handleSubmit(onSubmit)} className="m-0 flex flex-col items-center w-3/5 xs:w-4/5 sm:w-4/5">
+                <h2 className="text-white text-5xl xs:text-3xl sm:text-4xl tracking-[.05em] mt-4 font-bold text-center">Create a Suburb</h2>
+                <div className="flex flex-col w-4/5 xs:w-full sm:w-full items-center mt-6">
+                    <div className="w-4/5 xs:w-full sm:w-full flex flex-row justify-items-start">
                         <label className="text-white">Suburb's name:</label>
                     </div>
                     <input
-                        className="rounded-sm w-4/5 mt-2 p-2"
+                        className="rounded-sm w-4/5 xs:w-full sm:w-full mt-2 p-2"
                         {...register("name",
                             {required: true}
                         )}
@@ -53,40 +53,40 @@ export default function AddSuburnForm({ setAdded, added }: Props) {
 
                     />
                     {errors.name?.type === "required" && (
-                        <div className="w-4/5 flex flex-row justify-items-start">
+                        <div className="w-4/5 xs:w-full sm:w-full flex flex-row justify-items-start">
                             <p className="text-red" role="alert">
                                 Name is required
                             </p>
                         </div>
                     )}
                 </div>
-                <div className="flex flex-col w-4/5 mt-4 items-center">
-                    <div className="w-4/5 flex flex-row justify-items-start">
+                <div className="flex flex-col w-4/5 xs:w-full sm:w-full mt-4 items-center">
+                    <div className="w-4/5 xs:w-full sm:w-full flex flex-row justify-items-start">
                         <label className="text-white">Suburb's postcode:</label>
                     </div>
                     <input
-                        className="rounded-sm w-4/5 mt-2 p-2"
+                        className="rounded-sm w-4/5 xs:w-full sm:w-full mt-2 p-2"
                         {...register("postcode",
                             {required: true}
                         )}
                         aria-invalid={errors.postcode ? "true" : "false"}
                     />
                     {errors.postcode?.type === "required" && (
-                        <div className="w-4/5 flex flex-row justify-items-start">
+                        <div className="w-4/5 xs:w-full sm:w-full flex flex-row justify-items-start">
                             <p className="text-red" role="alert">Postcode is required</p>
                         </div>
                     )}
                 </div>
-                <div className="flex flex-col w-4/5 mt-8 items-center">
+                <div className="flex flex-col w-4/5 xs:w-full sm:w-full mt-8 items-center">
                     <input
-                        className="bg-fucsia p-3 text-white rounded-sm uppercase tracking-[.50em] w-4/5 hover:bg-fucsiaHover cursor-pointer"
+                        className="bg-fucsia p-3 text-white rounded-sm uppercase tracking-[.50em] w-4/5 xs:w-full sm:w-full hover:bg-fucsiaHover cursor-pointer"
                         type="submit"
                         value="Save"
                     />
                 </div>
-                <div className="flex flex-col w-4/5 mt-6 items-center">
+                <div className="flex flex-col w-4/5 xs:w-full sm:w-full mt-6 items-center">
                     <input
-                        className="bg-white p-3 text-dark rounded-sm w-4/5 mb-6 tracking-[.25em] hover:bg-grey cursor-pointer"
+                        className="bg-white p-3 text-dark rounded-sm w-4/5 xs:w-full sm:w-full mb-6 tracking-[.25em] hover:bg-grey cursor-pointer"
                         type="button"
                         onClick={() => reset()}
                         value="Reset"
