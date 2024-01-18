979e4b5ee662f1f23ba3957d431a1283b1f238c5
marcelamejiao <mejiao24@hotmail.com>
2024-01-18 17:50:41 +1100

    modify services adding implementation for login

--- src/services/suburbs.ts
+++ src/services/suburbs.ts
@@ -3,8 +3,16 @@ import Suburb from "../models/suburb";
 
 const apiHost: string = import.meta.env.VITE_API_HOST ?? 'http://localhost:8080';
 
+const credentials = localStorage.getItem("credentials");
+
 export const getAllSuburbs = async (): Promise<Suburb[]> => {
-    const response = await fetch(`${apiHost}/suburbs`);
+    const response = await fetch(`${apiHost}/suburbs`, {
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Basic ${credentials}`
+        },
+    });
     return await response.json();
 };
 
@@ -13,6 +21,8 @@ export const createSuburb = async (data: IFormValues) => {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Basic ${credentials}`
         },
         body: JSON.stringify(data),
     });
@@ -25,23 +35,41 @@ export const createSuburb = async (data: IFormValues) => {
 export const getSuburbsByPostcode = async (postcode: string): Promise<Array<Suburb>> => {
     const response = await fetch(`${apiHost}/suburbs/by-postcode?` + new URLSearchParams({
         postcode: postcode,
-    }));
+    }), {
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Basic ${credentials}`
+        },
+    } );
     return await response.json();
 };
 
 export const getSuburbsByName = async (name: string): Promise<Array<Suburb>> => {
     const response = await fetch(`${apiHost}/suburbs/by-name?` + new URLSearchParams({
         name: name,
-    }));
+    }), {
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Basic ${credentials}`
+        },
+    });
     return await response.json();
 };
 
 export const deleteSuburb = async (id: number) => {
     const response = await fetch(`${apiHost}/suburbs/${id}`, {
         method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Basic ${credentials}`
+        },
     });
 
     if (!response.ok) {
         throw new Error("Could not delete this suburb");
     }
 };
+
